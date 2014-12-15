7556d0446bfb1a5d60108e2185c9beeeca233611
Patrick Read <pread13@gmail.com>
2014-12-14 19:38:46 -0500

    altered git aliases

--- bash_profile_4.sh
+++ bash_profile_4.sh
@@ -1,9 +1,16 @@
 
-alias gpush="git push origin master"
-alias cdd="cd ~/Detroit\ Labs"
+# Edit: Reference the downloaded .git-prompt.sh file located in our home directory
+source ~/.git-prompt.sh
+
+# git aliases
+
+alias gpush="git push origin head"
+alias gpushh="git push heroku master"
+alias gpushp="git push production head"
 alias gs="git status -s"
 alias gb="git branch"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+alias gd="git diff"
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
